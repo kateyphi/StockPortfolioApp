0cfc57aad01c820c36c490ed80adff39b4b4c262
kateyphi <kateyphi@gmail.com>
2020-03-04 17:54:33 -0500

    font

--- client/components/Home.js
+++ client/components/Home.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
     return (
         <div>
-            <h1>Welcome to Play the Mock Market!</h1>
+            <h5>Welcome to Play the Mock Market!</h5>
             <p>Here, you can pretend to play the stock market. Just buy stocks and watch their value go up! (or down! both can happen!)</p>
             <p>To start buying stocks, either register a new account or sign in if you have one already.</p>
         </div>
